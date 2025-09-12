34442e240dd48b1bc0490fc59c7ff6d4b587f1b5
ohmycloud <ohmycloudy@gmail.com>
2025-09-12 17:44:04 +0800

    update user password

--- src/handler/user.rs
+++ src/handler/user.rs
@@ -7,9 +7,12 @@ use validator::Validate;
 use crate::{
     AppState,
     db::UserExt,
-    dtos::{FilterUserDto, NamedUpdateDto, UserData, UserResponseDto},
-    error::HttpError,
+    dtos::{
+        FilterUserDto, NamedUpdateDto, Response, UserData, UserPasswordUpdateDto, UserResponseDto,
+    },
+    error::{ErrorMessage, HttpError},
     middleware::JwtAuthMiddleware,
+    utils::password,
 };
 
 pub async fn update_user_name(
@@ -33,6 +36,49 @@ pub async fn update_user_name(
         data: UserData {
             user: filtered_user,
         },
+    };
+
+    Ok(Json(response))
+}
+
+pub async fn update_user_password(
+    Extension(app_state): Extension<Arc<AppState>>,
+    Extension(middleware): Extension<JwtAuthMiddleware>,
+    Json(body): Json<UserPasswordUpdateDto>,
+) -> Result<impl IntoResponse, HttpError> {
+    body.validate()
+        .map_err(|err| HttpError::bad_request(err.to_string()))?;
+    let user = &middleware.user;
+    let user_id = Uuid::parse_str(&user.id.to_string()).unwrap();
+    let user = app_state
+        .db_client
+        .get_user(Some(user_id.clone()), None, None)
+        .await
+        .map_err(|err| HttpError::server_error(err.to_string()))?;
+    let user = user.ok_or(HttpError::unauthorized(
+        ErrorMessage::InvalidToken.to_string(),
+    ))?;
+
+    let password_match = password::compare(&body.old_password, &user.password)
+        .map_err(|err| HttpError::server_error(err.to_string()))?;
+    if !password_match {
+        return Err(HttpError::bad_request(
+            "Old password is incorrect".to_string(),
+        ));
+    }
+
+    let hashed_password = password::hash(&body.new_password)
+        .map_err(|err| HttpError::server_error(err.to_string()))?;
+
+    app_state
+        .db_client
+        .update_user_password(user_id.clone(), hashed_password)
+        .await
+        .map_err(|err| HttpError::server_error(err.to_string()))?;
+
+    let response = Response {
+        status: "successful",
+        message: "Password updated successfully".to_string(),
     };
 
     Ok(Json(response))
